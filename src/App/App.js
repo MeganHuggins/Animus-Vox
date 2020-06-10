573248aeb94329a96312ccf6bc91a078b09e7efa
Megan Huggins <Muggins1265@gmail.com>
2020-06-09 19:13:28 -0600

    Add final tests/ clean up code

--- src/App/App.js
+++ src/App/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
         userInfo: {
           userName: ''
         },
-      isLoggedIn: true,
+      isLoggedIn: false,
       moods: [{type: 'Sad/Introspective', id: 0, statement:'Let’s Let It Out' }, {type: 'Happy/Energetic', id: 1, statement: 'Let’s Get Hyped'}, {type: 'Chill/Focused', id: 2, statement:'Let’s Chill Out'}, {type: 'Angry/Rebellious', id: 3, statement: 'Let’s Rage' }],
       currentMood: '',
     }
